f45eba93e226e5eea3b0a6fda6ebabb57da6a0b3
Caroline Andrade <andradecostacaroline@gmail.com>
2022-11-18 22:36:49 -0300

    resolução completa

--- src/main/kotlin/reservaCinema.kt
+++ src/main/kotlin/reservaCinema.kt
@@ -6,31 +6,29 @@ fun main() {
 fun populaCadeiras(): MutableList<String> {
     val fila = mutableListOf<String>()
     for (i in 0..10) {
-        fila.add("[b$i]")
+        fila.add("b$i")
     }
     println(fila)
     return fila
 }
-
 fun reservaCadeira(fila: MutableList<String>) {
     var resp = "s"
 
     while (resp == "s") {
         println("Escolha a cadeira:")
-        val escolha: String = "[${readLine().toString()}]"
+        val escolha: String = readLine().toString()
 
-        for (i in 0..10) {
-            if (escolha == fila[i]) {
-                fila[i] = "[--]"
-                println("Cadeira Reservada")
-            }else if(escolha == "[--]"){
-                println("ERRO! Lugar ocupado!")
-            }
+        val i = escolha.substringAfter("b").toInt()
+
+        if (fila[i] == escolha) {
+            fila[i] = "--"
+            println("Cadeira Reservada")
+        }else if (fila[i] == "--") {
+            println("ERRO! Lugar ocupado!")
         }
+
         println(fila)
         println("Quer reservar outra cadeira? [n]- nao [s]- sim")
         resp = readLine().toString()
     }
 }
-
-// solução incompleta
